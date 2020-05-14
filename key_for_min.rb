9068bcf17c6a25ba9c1ff55799fc78e41cd8465a
Lantz Warrick <betalantz@gmail.com>
2020-05-14 23:00:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,16 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  min_key, min_val = hash.first
+    # k_v_arr = hash.first
+    # min_key = k_v_arr[0]
+    # min_val = k_v_arr[1]
+    hash.each do |k, v|
+      if v < min_val
+        min_val = v
+        min_key = k
+        binding.pry
+      end
+    end
+    min_key
 end
