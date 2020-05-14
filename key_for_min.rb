430775d1093f4ab1c5dc26db06c31c22d61b343f
Lantz Warrick <betalantz@gmail.com>
2020-05-14 23:45:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@ require "pry"
 
 def key_for_min_value(name_hash)
   min_key, min_val = [nil, nil]
-    # k_v_arr = hash.first
+    # k_v_arr = [nil, nil]
     # min_key = k_v_arr[0]
     # min_val = k_v_arr[1]
     name_hash.each do |k, v|
